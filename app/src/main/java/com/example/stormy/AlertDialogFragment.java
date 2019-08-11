ad3e7d64f472e1da756e129e0ca7d72f8c1c46d6
Derrick Showers <derrick@derrickshowers.com>
2019-08-11 14:49:31 -0400

    Make call to Dark Sky API

---
+++ app/src/main/java/com/example/stormy/AlertDialogFragment.java
@@ -0,0 +1,23 @@
+package com.example.stormy;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import androidx.fragment.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
+import org.jetbrains.annotations.NotNull;
+
+public class AlertDialogFragment extends DialogFragment {
+
+    @NotNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .setTitle(R.string.error_title)
+                .setMessage(R.string.error_message)
+                .setPositiveButton(R.string.error_button_ok, null);
+        return builder.create();
+    }
+}
--- app/src/main/java/com/example/stormy/MainActivity.java
+++ app/src/main/java/com/example/stormy/MainActivity.java
@@ -2,13 +2,83 @@ package com.example.stormy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (isNetworkAvailable()) {
+            fireRequest();
+        } else {
+            Toast.makeText(this, getString(R.string.error_network_unavailable), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void fireRequest() {
+        String apiKey = "000";
+        double latitude = 40.795335;
+        double longitude = -73.972640;
+        String forecastURL = "https://api.darksky.net/forecast/"
+                + apiKey
+                + "/"
+                + latitude
+                + ","
+                + longitude;
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(forecastURL).build();
+
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                Log.e(TAG, "IO Exception Caught: ", e);
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                Log.v(TAG, response.body().string());
+                if (!response.isSuccessful()) {
+                    alertUserAboutError();
+                }
+            }
+        });
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager != null ? manager.getActiveNetworkInfo() : null;
+
+        boolean isAvailable = false;
+        if (networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        }
+
+        return isAvailable;
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getSupportFragmentManager(), "error_dialog");
     }
 }
